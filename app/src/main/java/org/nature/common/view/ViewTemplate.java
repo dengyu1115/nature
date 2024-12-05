1b05eb431df7724147f995f18ad399a758b004da
Nature <359032825@qq.com>
2024-12-05 08:56:42 +0800

    简化、规划代码、重命名

--- app/src/main/java/org/nature/common/view/ViewTemplate.java
+++ app/src/main/java/org/nature/common/view/ViewTemplate.java
@@ -13,9 +13,7 @@ import org.nature.R;
 import org.nature.common.constant.Const;
 import org.nature.common.util.DateUtil;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 import static android.text.InputType.*;
 
@@ -28,6 +26,7 @@ import static android.text.InputType.*;
 @SuppressLint({"UseCompatLoadingForDrawables", "ResourceType", "DefaultLocale"})
 public class ViewTemplate {
 
+    private static final Map<Context, ViewTemplate> MAP = new HashMap<>();
     /**
      * 默认内边距
      */
@@ -48,7 +47,11 @@ public class ViewTemplate {
     }
 
     public static ViewTemplate build(Context context) {
-        return new ViewTemplate(context);
+        ViewTemplate instance = MAP.get(context);
+        if (instance == null) {
+            MAP.put(context, instance = new ViewTemplate(context));
+        }
+        return instance;
     }
 
     /**
