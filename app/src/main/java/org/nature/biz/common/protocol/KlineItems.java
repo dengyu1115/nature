7f4e13b9700840b6e0bcae238b99ec65fe4c932f
Nature <359032825@qq.com>
2025-06-03 19:08:43 +0800

    完善注释

--- app/src/main/java/org/nature/biz/common/protocol/KlineItems.java
+++ app/src/main/java/org/nature/biz/common/protocol/KlineItems.java
@@ -4,7 +4,18 @@ import org.nature.biz.common.model.KInfo;
 
 import java.util.List;
 
+/**
+ * K线数据接口
+ * 用于定义获取K线信息的方法。
+ * @author Nature
+ * @version 1.0.0
+ * @since 2024/1/7
+ */
 public interface KlineItems {
 
+    /**
+     * 获取K线数据列表
+     * @return 返回包含KInfo对象的列表，每个对象代表一个K线数据点
+     */
     List<KInfo> kItems();
 }
--- app/src/main/java/org/nature/biz/common/protocol/NetItems.java
+++ app/src/main/java/org/nature/biz/common/protocol/NetItems.java
@@ -4,7 +4,18 @@ import org.nature.biz.common.model.NInfo;
 
 import java.util.List;
 
+/**
+ * 网络数据接口
+ * 用于定义获取网络信息的方法。
+ * @author Nature
+ * @version 1.0.0
+ * @since 2024/1/7
+ */
 public interface NetItems {
 
+    /**
+     * 获取网络数据列表
+     * @return 返回包含NInfo对象的列表，每个对象代表一个网络数据点
+     */
     List<NInfo> nItems();
 }
