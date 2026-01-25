89206cf2e28157b9844305945cb6bd94fe29653a
Nature <359032825@qq.com>
2026-01-25 20:38:03 +0800

    增加对象调用示例类

---
+++ app/src/main/java/org/nature/html/PyJava.java
@@ -0,0 +1,13 @@
+package org.nature.html;
+
+public class PyJava {
+
+    public String sayHello(String name) {
+        return "hello " + name;
+    }
+
+    public static String sayBye(String name) {
+        return "bye " + name;
+    }
+
+}
